036473c8b0490c0cfee36699f4e82dc09abbd29f
DavidsonCo <opeoluwa313@gmail.com>
2022-08-12 08:49:07 +0100

    Added more security features

--- shoshomedia/settings.py
+++ shoshomedia/settings.py
@@ -196,3 +196,6 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
